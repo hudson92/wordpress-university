da6809efe5839452fd369dd1994bc5b067f297e2
Sam H <samhudson1992@gmail.com>
2020-10-16 14:41:38 -0600

    Oct 16 Search JS updates

--- fake-university/app/public/wp-content/themes/university-theme/functions.php
+++ fake-university/app/public/wp-content/themes/university-theme/functions.php
@@ -40,8 +40,8 @@ function university_files() {
     wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
   } else {
     wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.24d997ebbc93d4ed3bb9.js'), NULL, '1.0', true);
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.1fe33bc8cdfe9a7a32fc.js'), NULL, '1.0', true);
-    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.1fe33bc8cdfe9a7a32fc.css'));
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.c18f6cdf27beb55f4e15.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.c18f6cdf27beb55f4e15.css'));
   }
 }
 
